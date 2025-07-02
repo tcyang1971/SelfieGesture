d732a230db2c6b0c4152a89e98726c8bf47afb8c
tcyang1971 <tcyang@gm.pu.edu.tw>
2025-07-02 14:10:45 +0800

    v1相機權限檢查

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("com.google.accompanist:accompanist-permissions:0.37.3")
+
 }
--- app/src/main/java/tw/edu/pu/csim/tcyang/selfiegesture/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/selfiegesture/MainActivity.kt
@@ -1,16 +1,26 @@
 package tw.edu.pu.csim.tcyang.selfiegesture
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
 import tw.edu.pu.csim.tcyang.selfiegesture.ui.theme.SelfieGestureTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +30,44 @@ class MainActivity : ComponentActivity() {
         setContent {
             SelfieGestureTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    CameraPermission()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun CameraPermission() {
+    // 獲取當前 Composable 的 Context
+    val context = LocalContext.current
+    // 使用 rememberPermissionState 記住相機權限的狀態
+    val permissionState = rememberPermissionState(
+        Manifest.permission.CAMERA
     )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SelfieGestureTheme {
-        Greeting("Android")
+    if (permissionState.status.isGranted) {
+        Text("您允許拍照權限，歡迎使用拍照功能！")
+    }
+    else{
+        if (permissionState.status.shouldShowRationale){
+            Button(onClick = {
+                permissionState.launchPermissionRequest()
+            })
+            {
+                Text("使用者拒絕但未勾選'不再詢問'，本應用程式需要相機權限才能正常運作，請授予權限。")
+            }
+        }
+        else{
+            Button(onClick = {
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                intent.data = Uri.fromParts("package", context.packageName, null)
+                context.startActivity(intent)
+            })
+            {
+                Text("使用者拒絕並勾選了'不再詢問'，或從未被詢問過。請到設定->應用程式，選擇App手動允許相機權限。")
+            }
+        }
     }
 }
